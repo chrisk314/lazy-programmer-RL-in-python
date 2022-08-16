9044584f8dcc36dd3a077294e827763190b0b861
Chris Knight <chrisk314@gmail.com>
2022-08-16 07:10:42 +0100

    Makes attributes private

--- exercises/ch5_DP/grid_world.py
+++ exercises/ch5_DP/grid_world.py
@@ -1,9 +1,9 @@
-from typing import Dict, List, Tuple
+from typing import Dict, Set, Tuple
 
 
 IntVec2d = Tuple[int, int]
 RewardsDict = Dict[IntVec2d, float]
-ActionsDict = Dict[IntVec2d, List[IntVec2d]]
+ActionsDict = Dict[IntVec2d, Set[IntVec2d]]
 
 REWARDS: RewardsDict = {
     (0, 3): 1,
@@ -11,15 +11,15 @@ REWARDS: RewardsDict = {
 }
 _U, _R, _D, _L = (-1, 0), (0, 1), (1, 0), (0, -1)
 ACTIONS: ActionsDict = {
-    (0, 0): [_R, _D],
-    (0, 1): [_R, _L],
-    (0, 2): [_R, _D, _L],
-    (1, 0): [_U, _D],
-    (1, 2): [_U, _R, _D],
-    (2, 0): [_U, _R],
-    (2, 1): [_R, _L],
-    (2, 2): [_U, _R, _L],
-    (2, 3): [_U, _L],
+    (0, 0): set([_R, _D]),
+    (0, 1): set([_R, _L]),
+    (0, 2): set([_R, _D, _L]),
+    (1, 0): set([_U, _D]),
+    (1, 2): set([_U, _R, _D]),
+    (2, 0): set([_U, _R]),
+    (2, 1): set([_R, _L]),
+    (2, 2): set([_U, _R, _L]),
+    (2, 3): set([_U, _L]),
 }
 
 
@@ -35,38 +35,41 @@ class GridWorld:
             rows: Number of rows in the grid (y axis).
             cols: Number of columns in the grid (x axis).
             start: Starting location in the grid.
+            actions: Possible actions for the agent.
+            rewards: Rewards for the states.
         """
-        self.rows = rows
-        self.cols = cols
-        self.state = start
-        self.actions = actions
-        self.rewards = rewards
-        self._active_states = set(self.actions.keys()) | set(self.rewards.keys())
+        self._rows: int = rows
+        self._cols: int = cols
+        self._state: IntVec2d = start
+        self._actions: ActionsDict = actions
+        self._rewards: RewardsDict = rewards
+
+    @property
+    def state(self) -> IntVec2d:
+        return self._state
 
     def _update_state(self, action: IntVec2d) -> IntVec2d:
-        i, j = self.state
+        i, j = self._state
         di, dj = action
-        new_state = (i + di, j + dj)
-        in_x = 0 <= new_state[0] < self.cols
-        in_y = 0 <= new_state[1] < self.rows
+        new__state = (i + di, j + dj)
+        in_x = 0 <= new__state[0] < self._cols
+        in_y = 0 <= new__state[1] < self._rows
         if in_x and in_y:
-            self.state = new_state
-            return new_state
-        raise ValueError("Action takes state out of bounds.")
+            self._state = new__state
+            return new__state
+        raise ValueError("Action takes _state out of bounds.")
 
     def act(self, action: IntVec2d) -> Tuple[IntVec2d, float]:
-        """Takes specified `action` and returns new state and reward."""
-        if action not in self.actions:
-            return self.state, 0.0
-        self.state = self._update_state(action)
-        reward = self.rewards.get(self.state)
-        if reward is None:
-            raise ValueError("Reached disallowed state!")
-        return self.state, reward
+        """Takes specified `action` and returns new _state and reward."""
+        if action not in self._actions[self._state]:
+            return self._state, 0.0
+        self._state = self._update_state(action)
+        reward = self._rewards.get(self._state, 0.0)
+        return self._state, reward
 
     def is_active(self) -> bool:
         """Returns True iff the episode is active."""
-        return self.state in self._active_states
+        return self._state in self._actions.keys()
 
 
 def policy(state: IntVec2d) -> IntVec2d:
